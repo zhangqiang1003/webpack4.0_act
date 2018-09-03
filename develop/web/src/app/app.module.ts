f39865a14dd316024802d7c0043ee6068422f912
zhangqiang <zhangqiang@qeeyou.cn>
2018-09-03 15:14:47 +0800

    添加测试组件和配置app.module.ts

--- develop/web/src/app/app.module.ts
+++ develop/web/src/app/app.module.ts
@@ -2,15 +2,62 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PLATFORM_ID, APP_ID, Injectable, Inject } from '@angular/core';
+import {
+  HttpClientModule,
+  HttpRequest,
+  HttpInterceptor,
+  HttpHandler,
+  HttpEvent,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { isPlatformBrowser, APP_BASE_HREF, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TipsComponent } from './components/tips/tips.component';
+import { Observable } from 'rxjs';
+@Injectable()
+export class Interceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const url = 'api请求地址';
 
+    req = req.clone({
+      url: url + req.url
+    });
+
+    return next.handle(req);
+  }
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'projectId'}),
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LocationStrategy, useClass: PathLocationStrategy},
+    { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
