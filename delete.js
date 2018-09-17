28b1a185562de5ba761e8f025d376ed48b439f3e
zhangqiang <zhangqiang@qeeyou.cn>
2018-09-17 19:50:16 +0800

    添加删除文件的逻辑

--- delete.js
+++ delete.js
@@ -1,17 +1,60 @@
-console.log('删除');
-console.log(process.argv);
 const fs = require('fs');
 const path = require('path');
 
 (function() {
-  let htmlPath = './src/specials/';
-  let staticPath = './src/static/specials/';
-  let type = process.argv[2]; // 判断是删除打包后上线版本的文件还是本地开发的源文件；值为：local 或 remote
-  console.log(type);
-  if (type !== 'local' && type !== 'remote') throw ('参数错误！');
-  let name = process.argv[3];
-  // 检查要删除的文件是否存在
-  let exists = fs.existsSync(path.resolve(__dirname, `${htmlPath}${name}.html`));
-  if (!exists) throw ('无法找到要删除的文件！');
-  console.log(name);
+  const htmlPath = './src/specials/'; // 本地开发环境的html资源路径
+  const staticPath = './src/static/specials/'; // 本地开发环境的静态资源路径
+  const htmlRemotePath = '../templates/specials/'; // 远端
+  const staticRemotePath = '../static/specials/';
+  const type = process.argv[2]; // 判断是删除打包后上线版本的文件还是本地开发的源文件；值为：local 或 remote
+  const name = process.argv[3]; // 要删除的文件名
+
+  // 先做一些检查
+  function check() {
+    if (type !== 'local' && type !== 'remote') throw ('参数错误！');
+    // 检查要删除的文件是否存在
+    const exists = fs.existsSync(path.resolve(__dirname, `${htmlPath}${name}.html`));
+    if (!exists) throw ('无法找到要删除的文件！');
+    if (type === 'local') {
+      delLocalFile();
+    } else if (type === 'remote') {
+      delRemoteFile();
+    }
+  }
+  check();
+  // 删除本地开发环境的源代码
+  function delLocalFile() {
+    fs.unlinkSync(path.resolve(__dirname, `${htmlPath}${name}.html`)); // 删除html文件
+    fs.unlinkSync(path.resolve(__dirname, `${staticPath}css/${name}.scss`)); // 删除scss文件
+    fs.unlinkSync(path.resolve(__dirname, `${staticPath}js/${name}.js`)); // 删除js文件
+
+    // 删除image文件
+    let imagesName = fs.readdirSync(path.resolve(__dirname, `${staticPath}images/`));
+    const reg = new RegExp(`${name}_`);
+    let filter = imagesName.filter((val) => {
+      return reg.test(val);
+    });
+    filter.forEach((val) => {
+      fs.unlinkSync(path.resolve(__dirname, `${staticPath}images/${val}`)); // 逐个删除image文件
+    });
+    console.log('文件删除成功！');
+  }
+
+  // 删除远端生产环境的打包代码
+  function delRemoteFile() {
+    fs.unlinkSync(path.resolve(__dirname, `${htmlRemotePath}${name}.html`)); // 删除html文件
+    fs.unlinkSync(path.resolve(__dirname, `${staticRemotePath}css/${name}.scss`)); // 删除scss文件
+    fs.unlinkSync(path.resolve(__dirname, `${staticRemotePath}js/${name}.js`)); // 删除js文件
+
+    // 删除image文件
+    let imagesName = fs.readdirSync(path.resolve(__dirname, `${staticRemotePath}images/`));
+    const reg = new RegExp(`${name}_`);
+    let filter = imagesName.filter((val) => {
+      return reg.test(val);
+    });
+    filter.forEach((val) => {
+      fs.unlinkSync(path.resolve(__dirname, `${staticRemotePath}images/${val}`)); // 逐个删除image文件
+    });
+    console.log('文件删除成功！');
+  }
 })();
